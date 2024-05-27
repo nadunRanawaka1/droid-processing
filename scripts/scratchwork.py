f3cb9025cc922669fa8d561a3899bde22471cde0
Nadun Kesara Ranawaka Arachchige <nkra3@sky1.cc.gatech.edu>
2024-05-27 18:05:13 -0400

    New filtered pickle files

--- scripts/scratchwork.py
+++ scripts/scratchwork.py
@@ -3,41 +3,82 @@ import nexusformat.nexus as nx
 import h5py
 import random
 
+demo_fn = "/nethome/nkra3/flash7/Droid/droid_hdf5/exp_datasets/pick_place_datasets/droid_1000_demos_small_spatial_diversity_agentview_mix.hdf5"
 
-demo_fn = "/media/nadun/Data/Droid/droid_hdf5/modified_datasets/droid_100_selected_agentview.hdf5"
-
-pickle_path = "/nethome/nkra3/flash7/Droid/droid-processing/droid_filter_keys/selected_agent_view_dict_droid_100.pkl"
+# pickle_path_1 = "/nethome/nkra3/flash7/Droid/droid-processing/droid_filter_keys/selected_agent_view_as_left_dict_droid_small_spatial.pkl"
+# pickle_path_2 = "/nethome/nkra3/flash7/Droid/droid-processing/droid_filter_keys/selected_agent_view_as_left_dict_droid_large_spatial.pkl"
 
 demo_file = nx.nxload(demo_fn)
 print(demo_file.tree)
-#
-# demo_file = h5py.File(demo_fn)
-#
-# demos = demo_file['data']
-#
+
+# demo_file = h5py.File(demo_fn, 'r')
+
+# masks = demo_file['mask']
+
+# small_spatial_demos = masks['pick_place_small_spatial'][:].tolist()
+# large_spatial_demos = masks['pick_place_large_spatial'][:].tolist()
+
+# for i, demo in enumerate(small_spatial_demos):
+#     small_spatial_demos[i] = str(demo, encoding='utf-8')
+
+# for i, demo in enumerate(large_spatial_demos):
+#     large_spatial_demos[i] = str(demo, encoding='utf-8')
+
+
+# print(small_spatial_demos)
+# print("============================================================")
+# print("============================================================")
+# print("============================================================")
+# print(large_spatial_demos)
+
+# random.shuffle(small_spatial_demos)
+# random.shuffle(large_spatial_demos)
+
+# selected_right_small_spatial = small_spatial_demos[:len(small_spatial_demos) // 2]
+# selected_left_small_spatial = small_spatial_demos[len(small_spatial_demos) // 2:]
+
+# selected_right_large_spatial = large_spatial_demos[:len(large_spatial_demos) // 2]
+# selected_left_large_spatial = large_spatial_demos[len(large_spatial_demos) // 2 : ]
+
+# selected_agentview_dict_small_spatial = {
+#     "shoulderview_left_image" : small_spatial_demos
+# }
+
+# selected_agentview_dict_large_spatial = {
+#     "shoulderview_left_image" : large_spatial_demos
+# }
+
+# with open(pickle_path_1, "wb") as f:
+#     pickle.dump(selected_agentview_dict_small_spatial, f)
+
+# with open(pickle_path_2, "wb") as f:
+#     pickle.dump(selected_agentview_dict_large_spatial, f)
+
+
+
 # demo_list = []
-#
+
 # for demo in demos:
 #     demo_list.append(demo)
-#
+
 # random.shuffle(demo_list)
-#
+
 # print(len(demo_list) // 2)
-#
+
 # selected_right = demo_list[:len(demo_list) // 2]
 # selected_left = demo_list[len(demo_list) // 2 :]
-#
+
 # selected_agent_view_dict = {
 #     "shoulderview_right_image" : selected_right,
 #     "shoulderview_left_image" : selected_left
 # }
 
 
-print(selected_agent_view_dict)
+# print(selected_agent_view_dict)
 
 demo_file.close()
 
-with open(pickle_path, "wb") as f:
-    pickle.dump(selected_agent_view_dict, f)
+# with open(pickle_path, "wb") as f:
+#     pickle.dump(selected_agent_view_dict, f)
 
 
--- scripts/select_agentview_from_dataset.py
+++ scripts/select_agentview_from_dataset.py
@@ -23,9 +23,7 @@ def select_agent_view_in_dict(agentview_dict_fn, dataset_path, processed_dataset
         num_written = 0
         src_dataset_grp = src_dataset['data']
 
-        if (num_written % 100) == 0:
-            print(f"Processed demo: {num_written}. Time elapsed: {time.time() - start}")
-
+        
         with h5py.File(processed_dataset_path, "w") as processed_dataset:
 
             processed_dataset_grp = processed_dataset.create_group("data")
@@ -34,6 +32,10 @@ def select_agent_view_in_dict(agentview_dict_fn, dataset_path, processed_dataset
                 selected_demos = agentview_dict[camera]
 
                 for demo in selected_demos:
+
+                    if (num_written % 20) == 0:
+                        print(f"Processed demo: {num_written}. Time elapsed: {time.time() - start}")
+
 
                     #First, copy all data from the source to the new processed dataset
                     processed_dataset_grp.copy(src_dataset_grp[demo], f"demo_{num_written}")
@@ -59,8 +61,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     select_agent_view_in_dict(args.agentview_dict_path, args.dataset_path, args.processed_dataset_path)
-
-
 
 
 
