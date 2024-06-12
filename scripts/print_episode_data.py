c8085f65639465fec177eaea9a962f11a65dcbfd
Nadun Ranawaka Arachchige <nadun.kesara@gmail.com>
2024-06-12 14:02:06 -0400

    Slight change

--- scripts/print_episode_data.py
+++ scripts/print_episode_data.py
@@ -1,8 +1,16 @@
 import tensorflow_datasets as tfds
 
 
-droid_path = "/media/nadun/Data/Droid/droid_100/1.0.0"
-builder = tfds.builder_from_directory(builder_dir=droid_path)
-print(builder.info.features)
+droid_path = "/media/nadun/Data/Droid"
+
+droid_ds = tfds.load("droid_100", data_dir=droid_path, split="train")
+
+for episode in droid_ds:
+    for step in episode['steps']:
+        obs = step['observation']
+        print(obs['exterior_image_1_left'])
+        print()
+    print()
+    break
 
 
