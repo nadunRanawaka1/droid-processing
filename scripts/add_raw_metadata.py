6cc22c830e77f3325fd323ee6149ebbdf5ce9cd2
Nadun Ranawaka Arachchige <nadun.kesara@gmail.com>
2024-08-19 22:35:22 -0400

    Fixed adding raw metadata

--- scripts/add_raw_metadata.py
+++ scripts/add_raw_metadata.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import json
 
-metadata_fp = "/nethome/nkra3/flash7/Droid/droid_hdf5/metadata/droid_metadata.pkl"
+metadata_fp = "/media/nadun/Data/Droid/droid_hdf5/metadata/droid_metadata.pkl"
 
 df = pd.read_pickle(metadata_fp)
 
@@ -14,11 +14,11 @@ df["scene_id"] = pd.Series(dtype="int")
 df["task"] = pd.Series(dtype="str")
 df["building"] = pd.Series(dtype="str")
 df["trajectory_length"] = pd.Series(dtype="int")
-df["wrist_cam_extrinsics"] = [[0.0 for i in range(7)] for j in range(len(df))]
-df["ext1_cam_extrinsics"] = [[0.0 for i in range(7)] for j in range(len(df))]
-df["ext2_cam_extrinsics"] = [[0.0 for i in range(7)] for j in range(len(df))]
+df["wrist_cam_extrinsics"] = [[] for j in range(len(df))]
+df["ext1_cam_extrinsics"] = [[] for j in range(len(df))]
+df["ext2_cam_extrinsics"] = [[] for j in range(len(df))]
 
-metadata_file_dir = "/nethome/nkra3/flash7/Droid/droid_hdf5/metadata/raw_metadata/raw_metadata_files"
+metadata_file_dir = "/media/nadun/Data/Droid/droid_hdf5/metadata/raw_metadata/raw_metadata_files"
 
 raw_metadata_file_names = os.listdir(metadata_file_dir)
 
@@ -37,18 +37,28 @@ for file in raw_metadata_file_names:
             continue
         index = demo_df['Demo'].index.values[0]
 
-        df[df['filepath'].str.contains(hdf5_path)]["lab"] = metadata["lab"]
-        df[df['filepath'].str.contains(hdf5_path)]["date"] = metadata["date"]
-        df[df['filepath'].str.contains(hdf5_path)]["timestamp"] = metadata["timestamp"]
-        df[df['filepath'].str.contains(hdf5_path)]["scene_id"] = metadata["scene_id"]
-        df[df['filepath'].str.contains(hdf5_path)]["task"] = metadata["current_task"]
-        df[df['filepath'].str.contains(hdf5_path)]["building"] = metadata["building"]
-        df[df['filepath'].str.contains(hdf5_path)]["trajectory_length"] = metadata["trajectory_length"]
+        # df[df['filepath'].str.contains(hdf5_path)]["lab"] = metadata["lab"]
+        # df[df['filepath'].str.contains(hdf5_path)]["date"] = metadata["date"]
+        # df[df['filepath'].str.contains(hdf5_path)]["timestamp"] = metadata["timestamp"]
+        # df[df['filepath'].str.contains(hdf5_path)]["scene_id"] = metadata["scene_id"]
+        # df[df['filepath'].str.contains(hdf5_path)]["task"] = metadata["current_task"]
+        # df[df['filepath'].str.contains(hdf5_path)]["building"] = metadata["building"]
+        # df[df['filepath'].str.contains(hdf5_path)]["trajectory_length"] = metadata["trajectory_length"]
+
+        df.loc[index, "lab"] = metadata["lab"]
+        df.loc[index, "date"] = metadata["date"]
+        df.loc[index, "timestamp"] = metadata["timestamp"]
+        df.loc[index, "scene_id"] = metadata["scene_id"]
+        df.loc[index, "task"] = metadata["current_task"]
+        df.loc[index, "building"] = metadata["building"]
+        df.loc[index, "trajectory_length"] = metadata["trajectory_length"]
+
+
         df.at[index, "wrist_cam_extrinsics"] = metadata["wrist_cam_extrinsics"]
         df.at[index, "ext1_cam_extrinsics"] = metadata["ext1_cam_extrinsics"]
         df.at[index, "ext2_cam_extrinsics"] = metadata["ext2_cam_extrinsics"]
 
 
-df.to_pickle("/nethome/nkra3/flash7/Droid/droid_hdf5/metadata/new_droid_metadata.pkl")
+df.to_pickle("new_droid_metadata_v3.pkl")
 
 
--- scripts/scratchwork.py
+++ scripts/scratchwork.py
@@ -5,25 +5,13 @@ import random
 import re
 import pandas as pd
 
-metadata_fp = "/media/nadun/Data/Droid/droid_hdf5/metadata/droid_metadata.pkl"
+metadata_fp = "/media/nadun/Data/Droid/droid_hdf5/metadata/new_droid_metadata.pkl"
 
 df = pd.read_pickle(metadata_fp)
 
 # unique_lang_1_counts = df["language_instruction_1"].value_counts()[1:] # index from 1 to remove empty string
 # unique_lang_2_counts = df["language_instruction_2"].value_counts()[1:]
 # unique_lang_3_counts = df["language_instruction_3"].value_counts()[1:]
-
-search_strings = ["cube", "block"]
-
-cube_mask = df['language_instruction_1'].str.contains('|'.join(search_strings), case=False) | df['language_instruction_2'].str.contains('|'.join(search_strings), case=False) | df['language_instruction_3'].str.contains('|'.join(search_strings), case=False)
-
-df = df[cube_mask]
-
-cube_demos = {}
-cube_demos["demos_with_cubes"] = df["Demo"].values.tolist()
-
-with open("/media/nadun/Data/Droid/droid-processing/droid_filter_keys/cube_demos_list.pkl", "wb") as f:
-    pickle.dump(cube_demos, f)
 
 print()
 
