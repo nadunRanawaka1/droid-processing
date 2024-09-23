60ae2d339fa4fa33f3ae8478db6200d2903f4b07
Nadun Ranawaka Arachchige <nadun.kesara@gmail.com>
2024-09-23 18:01:46 -0400

    Cleanup

--- scripts/add_demos_with_lang_con.py
+++ scripts/add_demos_with_lang_con.py
@@ -1,18 +1,23 @@
 import pickle
-from robomimic.utils.file_utils import create_hdf5_filter_key
+# from robomimic.utils.file_utils import create_hdf5_filter_key
 
 metadata_fp = "/media/nadun/Data/Droid/metadata/droid_metadata/all_droid_metadata_with_pick_and_place_tasks.pkl"
+save_path = "/media/nadun/Data/Droid/metadata/successful_demos_with_lang.pkl"
 
 with open(metadata_fp, "rb") as f:
     df = pickle.load(f)
 
 filtered_df = df[df['language_instruction_1'].str.strip() != '']
 
-demos = filtered_df['Demo'].tolist()
-filter_key = "successful_with_lang"
-droid_path = ""
+demo_list = filtered_df['Demo'].tolist()
 
-ep_lengths = create_hdf5_filter_key(droid_path, demos, filter_key)
+with open(save_path, "wb") as f:
+    pickle.dump(demo_list, f)
+
+# filter_key = "successful_with_lang"
+# droid_path = ""
+#
+# ep_lengths = create_hdf5_filter_key(droid_path, demos, filter_key)
 
 
 print()
